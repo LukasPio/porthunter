b64c86e4eeb8ca87ce6866ac90bcdc410f0f43af
LukasPio <lucaspio.galvao@gmail.com>
2026-06-27 19:06:53 -0300

    porthunter v1

--- main.go
+++ main.go
@@ -5,25 +5,83 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 )
 
+var targetIp string
+
 func main() {
 
-	targetIp := os.Args[1]
+	if len(os.Args) < 4 {
+		println("Missing parameters\nUsage: porthunter <ip-address> <range-start> <range-end>\nExample: porthunter 192.168.0.1 1 100 - ports across 1 and 100 on 192.168.0.1")
+		os.Exit(1)
+	}
 
-	ports := [7]int{21, 22, 80, 3000, 3306, 8080, 443}
+	targetIp = os.Args[1]
 
-	for i := 0; i < len(ports); i++ {
+	start, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid start range")
+		os.Exit(1)
+	}
+	end, err := strconv.Atoi(os.Args[3])
+	if err != nil || end > 65535 {
+		fmt.Println("Invalid end range")
+		os.Exit(1)
+	}
 
-		currentPort := strconv.Itoa(ports[i])
+	if start <= 0 || end <= 0 {
+		fmt.Println("Ports can't be 0 or negative")
+		os.Exit(1)
+	}
 
-		connection, error := net.DialTimeout("tcp", targetIp + ":" + currentPort, 2*time.Second)
+	if start > end {
+		fmt.Println("Start range must be less than or equal to end")
+		os.Exit(1)
+	}
+
+	workersAmount := (end - start) / 50
+
+	if workersAmount < 1 {
+    	workersAmount = 1
+	}
+
+	if workersAmount > 1000 {
+    	workersAmount = 1000
+	}
+
+	var wg sync.WaitGroup
+	ports := make(chan int, workersAmount)
+
+	for i := 0; i < workersAmount; i++ {
+		wg.Add(1)
+		go scanPorts(ports, &wg)
+	}
+
+	fmt.Println("Scanning for ports on " + targetIp)
+
+	for i := start; i < end+1; i++ {
+		ports <- i
+	}
+
+	close(ports)
+
+	wg.Wait()
+}
+
+func scanPorts(ports <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for port := range ports {
+
+		portToString := strconv.Itoa(port)
+
+		connection, error := net.DialTimeout("tcp", targetIp+":"+portToString, 2*time.Second)
 
 		if error == nil {
 			connection.Close()
-			fmt.Println("Porta " + currentPort + " aberta!")
+			fmt.Println("Porta " + portToString + " aberta!")
 		}
 	}
-
 }
